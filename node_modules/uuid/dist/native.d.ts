e6d0690175b9b205ce869c206ef343518e90be56
vinitsingare <vinitsingare05@gmail.com>
2025-09-19 22:21:36 +0530

    Ignore node_modules

--- node_modules/uuid/dist/native.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="node" resolution-mode="require"/>
-import { randomUUID } from 'node:crypto';
-declare const _default: {
-    randomUUID: typeof randomUUID;
-};
-export default _default;
